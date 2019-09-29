02a76b8721fa949711f3f70e71cfa6de89f86bcb
braniii <daniel.nagel@protonmail.com>
2019-09-29 19:15:04 +0200

    Initial upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setuptools.setup(
+    name="prettypyplot",
+    version="0.1",
+    description="Wrapper for matplotlib to generate pretty plots.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    keywords="matplotlib pyplot",
+    url="https://gitlab.com/braniii/prettyplot",
+    author="braniii",
+    author_email="",
+    license="BSD 3-Clause License",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=['prettypyplot'],
+    include_package_data=True,
+    install_requires=["matplotlib", "numpy"],
+)
