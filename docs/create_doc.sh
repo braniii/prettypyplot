98399439283ee4532a494f7edafd6a8a121b6fe5
braniii <daniel.nagel@protonmail.com>
2020-01-31 01:08:51 +0100

    Automize doc creation.

--- docs/create_doc.sh
+++ docs/create_doc.sh
@@ -1,3 +1,24 @@
-pdoc --html -o . --template-dir . --force ../prettypyplot/
+# generate doc
+echo 'create documentation'
+pdoc --html -o . --template-dir ./config --force ../prettypyplot
+
+mv prettypyplot/cmaps/* ./cmaps/
+rmdir prettypyplot/cmaps
+
 mv prettypyplot/* .
 rmdir prettypyplot
+
+# replace MPL_DOC with link
+sed -i -e 's/MPL_DOC/https:\/\/matplotlib.org\/api\/_as_gen\/matplotlib/g' *.html
+echo ''
+
+# generate figures in gallery
+cd ../
+echo 'recreate gallery figures:'
+for file in $(find ./gallery/ -name '*.py')
+do
+    echo '   exec: '$file
+    python3 $file
+done
+
+cd docs
