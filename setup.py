cd9deff0c70a940f2eb5e66b3ae22a5002e1a936
braniii <daniel.nagel@protonmail.com>
2020-02-04 22:30:55 +0100

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / 'README.md').read_text()
 # This call to setup() does all the work
 setuptools.setup(
     name='prettypyplot',
-    version='0.2.2',
+    version='0.2.3',
     description='Wrapper for matplotlib to generate pretty plots.',
     long_description=README,
     long_description_content_type='text/markdown',
