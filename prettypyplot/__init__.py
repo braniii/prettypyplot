97e0faddeaf1263ac3d821b1287b2211405dc6c5
braniii <daniel.nagel@protonmail.com>
2020-04-21 13:32:33 +0200

    Bump version: 0.3.0 → 0.4.0

--- prettypyplot/__init__.py
+++ prettypyplot/__init__.py
@@ -6,4 +6,4 @@
 from .colors import *
 from .plot import *
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / 'README.md').read_text()
 # This call to setup() does all the work
 setuptools.setup(
     name='prettypyplot',
-    version='0.3.0',
+    version='0.4.0',
     description='Wrapper for matplotlib to generate pretty plots.',
     long_description=README,
     long_description_content_type='text/markdown',
