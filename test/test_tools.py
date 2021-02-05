1abce6d337a99921347985e117a2dc2661721d08
braniii <daniel.nagel@protonmail.com>
2021-02-05 17:04:43 +0100

    Add tests for tools submodule.

--- test/test_tools.py
+++ test/test_tools.py
@@ -6,8 +6,10 @@ Copyright (c) 2019-2020, Daniel Nagel
 All rights reserved.
 
 """
+import matplotlib as mpl
 import numpy as np
 import pytest
+from matplotlib import pyplot as plt
 
 import prettypyplot
 
@@ -26,4 +28,104 @@ def test_parse_figratio(figratio, refratio, error):
         np.testing.assert_almost_equal(figratio, refratio)
     else:
         with pytest.raises(error):
-            figratio = prettypyplot.tools.parse_figratio(figratio)
+            prettypyplot.tools.parse_figratio(figratio)
+
+
+@pytest.mark.parametrize('figsize, figratio, refsize, error', [
+    ((1, 2), None, (1, 2), None),
+    ([1, 2], None, (1, 2), None),
+    (np.array([1, 2]), None, (1, 2), None),
+    (1, 0.5, (1, 2), None),
+    ((1, ), 0.5, (1, 2), None),
+    ([1, ], 0.5, (1, 2), None),
+    ((1, 2, 3), None, None, ValueError),
+    ('a', None, None, ValueError),
+    (('a', 2), None, None, ValueError),
+])
+def test_parse_figsize(figsize, figratio, refsize, error):
+    """Test parsing figsize."""
+    if error is None:
+        figsize = prettypyplot.tools.parse_figsize(figsize, figratio)
+        np.testing.assert_array_almost_equal(figsize, refsize)
+    else:
+        with pytest.raises(error):
+            prettypyplot.tools.parse_figsize(figsize, figratio)
+
+
+@pytest.mark.parametrize('number, is_number', [
+    (1, True),
+    (1.0, True),
+    (True, True),
+    (False, True),
+    ('a', False),
+    ((1, 2), False),
+])
+def test_is_number(number, is_number):
+    """Test is_number."""
+    assert is_number == prettypyplot.tools.is_number(number)
+
+
+@pytest.mark.parametrize('number, refnumber, error', [
+    (1, -1, None),
+    (-1, 1, None),
+    ('-1', '1', None),
+    ('5%', '-5%', None),
+    ((1, 2), None, ValueError),
+])
+def test_invert_sign(number, refnumber, error):
+    """Test inverting sign."""
+    if error is None:
+        assert refnumber == prettypyplot.tools.invert_sign(number)
+    else:
+        with pytest.raises(error):
+            prettypyplot.tools.invert_sign(number)
+
+
+def test_parse_axes():
+    """Ensure, that gca returns always an axes instance."""
+    fig, ax = plt.subplots()
+
+    # parse correct ax in ax
+    argsref = (1, 'a', np.arange(2))
+    argsax = prettypyplot.tools.parse_axes(*argsref, ax=ax)
+    assert all(
+        isinstance(ref, type(parse))
+        for ref, parse in zip(argsref, argsax[0])
+    )
+    assert ax is argsax[1]
+
+    # multiple axes
+    with pytest.raises(ValueError):
+        prettypyplot.tools.parse_axes(ax, ax=ax)
+    with pytest.raises(ValueError):
+        prettypyplot.tools.parse_axes(1, ax, ax=ax)
+    with pytest.raises(ValueError):
+        prettypyplot.tools.parse_axes(ax, ax, 1, ax=None)
+
+    argsax = prettypyplot.tools.parse_axes(ax, ax=None)
+    assert ax is argsax[1]
+
+    argsax = prettypyplot.tools.parse_axes(ax=ax)
+    assert ax is argsax[1]
+
+
+def test_gca():
+    """Ensure, that gca returns always an axes instance."""
+    fig, ax = plt.subplots()
+    assert isinstance(prettypyplot.tools.gca(None), mpl.axes.Axes)
+    assert isinstance(prettypyplot.tools.gca(plt.gca()), mpl.axes.Axes)
+
+
+def test_get_axes():
+    """Ensure, that get_axes returns always an axes instance."""
+    fig, axs = plt.subplots()
+    assert all(
+        isinstance(ax, mpl.axes.Axes)
+        for ax in prettypyplot.tools.get_axes(axs)
+    )
+    assert all(
+        isinstance(ax, mpl.axes.Axes)
+        for ax in prettypyplot.tools.get_axes(None)
+    )
+    with pytest.raises(TypeError):
+        prettypyplot.tools.get_axes(fig)
