066e8d3de981b430677ae061883fb005890bf120
braniii <daniel.nagel@protonmail.com>
2021-02-09 11:56:37 +0100

    Add test for creating cmaps.

---
+++ test/test_cmaps.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""Tests for the cmaps module.
+
+BSD 3-Clause License
+Copyright (c) 2019-2020, Daniel Nagel
+All rights reserved.
+
+"""
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
+
+from prettypyplot import cmaps
+
+
+def test_bownair():
+    """Test bownair cmap."""
+    assert isinstance(
+        cmaps._bownair._bownair(), LinearSegmentedColormap,
+    )
+
+
+def test_macaw():
+    """Test macaw cmap."""
+    assert isinstance(
+        cmaps._macaw._macaw(), LinearSegmentedColormap,
+    )
+
+
+def test_turbo():
+    """Test turbo cmap."""
+    assert isinstance(
+        cmaps._turbo._turbo(), LinearSegmentedColormap,
+    )
+
+
+def test_discrete():
+    """Test discrete cmaps."""
+    for cmap in (
+        cmaps._discrete._pastel5(),
+        cmaps._discrete._pastel6(),
+        cmaps._discrete._cbf4(),
+        cmaps._discrete._cbf5(),
+        cmaps._discrete._cbf8(),
+        cmaps._discrete._pastel_rainbow(),
+        cmaps._discrete._pastel_spring(),
+        cmaps._discrete._pastel_autunm(),
+        cmaps._discrete._ufcd(),
+        cmaps._discrete._paula(),
+    ):
+        assert isinstance(cmap, ListedColormap)
