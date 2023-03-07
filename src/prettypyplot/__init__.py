f879a2d621451312f6b8055487edf075090bfbde
braniii <daniel.nagel@protonmail.com>
2023-03-07 22:09:36 +0100

    Add brief reference overview.

--- src/prettypyplot/__init__.py
+++ src/prettypyplot/__init__.py
@@ -1,4 +1,25 @@
-"""--8<-- "README.md" """
+"""# Prettypyplot
+
+This package provides helper functions to easen the usage of matplotlib.
+
+The module is structured into the following submodules:
+
+- [**pyplot:**][prettypyplot.pyplot] This submodule contains all methods
+  related to plotting inside a single axes, so basically related to
+  [matplotlib.pyplot][].
+
+- [**style:**][prettypyplot.style] This module provides only method to load
+  and alter the current style.
+
+- [**subplots:**][prettypyplot.subplots] This module provides methods to
+  simplify dealing with [matplotlib.pyplot.subplots][] grids.
+
+- [**texts:**][prettypyplot.texts] This module provides methods to
+  plot text with the possibility to add a contour.
+
+- [**tools:**][prettypyplot.tools] This module provides uitility methods to.
+
+"""
 # both are set in style submodule to default value
 MODE = None
 STYLE = None
@@ -6,10 +27,34 @@ STYLE = None
 # style dictionary
 STYLE_DICT = {}
 
+__all__ = [
+    'setup_pyplot',
+    'update_style',
+    'use_style',
+    'add_contour',
+    'text',
+    'figtext',
+    'hide_empty_axes',
+    'label_outer',
+    'subplot_labels',
+    'load_cmaps',
+    'load_colors',
+    'categorical_cmaps',
+    'categorical_color',
+    'text_color',
+]
+
 from .colors import *
-from .pyplot import *
-from .style import *
-from .texts import *
-from .subplots import *
+from .pyplot import (
+    imshow,
+    plot,
+    savefig,
+    legend,
+    colorbar,
+    grid,
+)
+from .style import (setup_pyplot, update_style, use_style)
+from .texts import (add_contour, figtext, text)
+from .subplots import (hide_empty_axes, label_outer, subplot_labels)
 
 __version__ = '0.9.0'
